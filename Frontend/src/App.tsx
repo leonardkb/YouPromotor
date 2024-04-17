0f51736aa53f136d67434ec257ed0be9308174cd
joeljaison <joeljaison394@gmail.com>
2024-04-17 10:14:14 +0530

    Page: added homepage

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,20 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Routes , Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import LandingPage from './pages/LandingPage'
 import AuthPage from './pages/AuthPage'
+import HomePage from './pages/HomePage'
 
 function App() {
 
 
   return (
     <>
-     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/auth" element={<AuthPage />} />
-     </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
     </>
   )
 }
